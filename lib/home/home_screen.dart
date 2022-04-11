9f4bde863879abcb9d7daee6dab5b7d9ddb2b5b8
Hriday Pradhan <hridayjyotipradhandc@gmail.com>
2022-04-11 16:56:09 +0530

    Basic home screen file setup

---
+++ lib/home/home_screen.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatelessWidget {
+  static const id = '/home_screen';
+
+  const HomeScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const SafeArea(
+      child: Scaffold(
+        body: Center(
+          child: Text('Home Screen'),
+        ),
+      ),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:get_journal/home/home_screen.dart';
 
 void main() => runApp(const GetJournal());
 
@@ -8,15 +9,11 @@ class GetJournal extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Material App',
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text('Material App Bar'),
-        ),
-        body: const Center(
-          child: Text('Hello World'),
-        ),
-      ),
+      debugShowCheckedModeBanner: false,
+      initialRoute: HomeScreen.id,
+      routes: {
+        HomeScreen.id: (context) => const HomeScreen(),
+      },
     );
   }
 }
