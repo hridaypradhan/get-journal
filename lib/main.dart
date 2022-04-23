520c9dcfc70d409ce6b868d0c6f0d7949f1e2d0f
Hriday Pradhan <hridayjyotipradhandc@gmail.com>
2022-04-23 19:47:26 +0530

    Added some constants and files

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,9 @@
 import 'package:flutter/material.dart';
-import 'package:get_journal/home/home_screen.dart';
+import 'package:get_journal/global/constants/colors.dart';
+import 'package:get_journal/screens/landing/landing_screen.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+import 'screens/home/home_screen.dart';
 
 void main() => runApp(const GetJournal());
 
@@ -9,10 +13,18 @@ class GetJournal extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      theme: ThemeData(
+        fontFamily: GoogleFonts.montserrat().fontFamily,
+        primaryColor: kPrimaryBlue,
+        colorScheme: Theme.of(context).colorScheme.copyWith(
+              secondary: kPrimaryBlue,
+            ),
+      ),
       debugShowCheckedModeBanner: false,
       initialRoute: HomeScreen.id,
       routes: {
         HomeScreen.id: (context) => const HomeScreen(),
+        LandingScreen.id: (context) => const LandingScreen(),
       },
     );
   }
---
+++ lib/screens/landing/landing_screen.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class LandingScreen extends StatelessWidget {
+  static const id = '/landing_screen';
+  const LandingScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      
+    );
+  }
+}
